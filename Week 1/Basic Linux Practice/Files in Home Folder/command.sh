9e208bd6748955707ff77ebe2dfa5cdabbf54cdc
Atul Kamble <atul_kamble@hotmail.com>
2022-07-11 20:39:56 +0530

    Upload
    Upload

---
+++ Week 1/Basic Linux Practice/Files in Home Folder/command.sh
@@ -0,0 +1,5 @@
+
+echo "THis is the bash command"
+
+sudo apt update
+sudo apt upgrade
---
+++ Week 1/Linux Bash Cheatsheet/Bash Scripts/variables.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+MSG="Hello World"
+echo "$MSG Alex" # Hello World Alex
+echo '$MSG Alex' # $MSG Alex
